f7a6d2ff58cae0fa7cf504edda79303d70afff48
mikel duke <mikelduke@gmail.com>
2018-06-06 20:29:40 -0500

    start adding test app

--- src/test/java/com/mikelduke/opentracing/sparkjava/OpentracingSparkTestApplication.java
+++ src/test/java/com/mikelduke/opentracing/sparkjava/OpentracingSparkTestApplication.java
@@ -1,5 +1,28 @@
 package com.mikelduke.opentracing.sparkjava;
 
-public class OpentracingSparkTestApplication {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import spark.Spark;
+
+public class OpentracingSparkTestApplication {
+    private static final String CLAZZ = OpentracingSparkTestApplication.class.getName();
+	private static final Logger LOGGER = Logger.getLogger(CLAZZ);
+
+    public static void main(String[] args) {
+        OpentracingSparkFilters sparkTracingFilters = new OpentracingSparkFilters();
+		Spark.before(sparkTracingFilters.before());
+		Spark.afterAfter(sparkTracingFilters.afterAfter());
+
+        Spark.exception(Exception.class, sparkTracingFilters.exception());
+
+		Spark.get("/", (req, res) -> "hello");
+
+		Spark.get("/error", (req, res) -> {
+			throw new RuntimeException("test exceotion");
+		});
+
+		Spark.awaitInitialization();
+		LOGGER.logp(Level.INFO, CLAZZ, "main", "Server started on port " + Spark.port());
+	}
 }
