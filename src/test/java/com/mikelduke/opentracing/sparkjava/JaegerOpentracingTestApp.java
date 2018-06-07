8098e36b27269c61b256a1b0365c21576d34556b
mikel duke <mikelduke@gmail.com>
2018-06-06 22:48:41 -0500

    test app using jaeger tracer

---
+++ src/test/java/com/mikelduke/opentracing/sparkjava/JaegerOpentracingTestApp.java
@@ -0,0 +1,33 @@
+package com.mikelduke.opentracing.sparkjava;
+
+import io.jaegertracing.Span;
+import io.jaegertracing.Tracer;
+import io.jaegertracing.reporters.InMemoryReporter;
+import io.jaegertracing.reporters.Reporter;
+import io.jaegertracing.samplers.ConstSampler;
+import io.jaegertracing.samplers.Sampler;
+import io.opentracing.util.GlobalTracer;
+
+public class JaegerOpentracingTestApp {
+
+	public static void main(String[] args) {
+
+        Reporter reporter = new InMemoryReporter() {
+            @Override
+            public void report(Span span) {
+                super.report(span);
+                System.out.println("Span Reported: " + span + " Tags: " + span.getTags());
+            }
+        };
+        
+        Sampler sampler = new ConstSampler(true);
+        Tracer tracer = new Tracer.Builder("sparkjava-test")
+                .withReporter(reporter)
+                .withSampler(sampler)
+                .build();
+
+        GlobalTracer.register(tracer);
+
+        new OpentracingSparkTestApplication().start();
+	}
+}
--- src/test/java/com/mikelduke/opentracing/sparkjava/OpentracingSparkTestApplication.java
+++ src/test/java/com/mikelduke/opentracing/sparkjava/OpentracingSparkTestApplication.java
@@ -3,20 +3,49 @@ package com.mikelduke.opentracing.sparkjava;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
 import spark.Spark;
 
 public class OpentracingSparkTestApplication {
     private static final String CLAZZ = OpentracingSparkTestApplication.class.getName();
 	private static final Logger LOGGER = Logger.getLogger(CLAZZ);
 
-    public static void main(String[] args) {
-        OpentracingSparkFilters sparkTracingFilters = new OpentracingSparkFilters();
+	private final Tracer tracer;
+
+	public static void main(String[] args) {
+		new OpentracingSparkTestApplication().start();
+	}
+
+	public OpentracingSparkTestApplication() {
+		this(GlobalTracer.get());
+	}
+
+	public OpentracingSparkTestApplication(Tracer tracer) {
+		this.tracer = tracer;
+	}
+
+	public void start() {
+		OpentracingSparkFilters sparkTracingFilters = new OpentracingSparkFilters();
 		Spark.before(sparkTracingFilters.before());
 		Spark.afterAfter(sparkTracingFilters.afterAfter());
-
         Spark.exception(Exception.class, sparkTracingFilters.exception());
 
 		Spark.get("/", (req, res) -> "hello");
+
+		Spark.get("/bad", (req, res) -> {
+			res.status(400);
+			res.body("bad request");
+			return res;
+		});
+
+		Spark.get("/child", (req, res) -> {
+			Span span = req.attribute(OpentracingSparkFilters.SERVER_SPAN);
+			tracer.buildSpan("child").asChildOf(span).withTag("test", "value").start().finish();
+
+			return "done";
+		  });
 
 		Spark.get("/error", (req, res) -> {
 			throw new RuntimeException("test exceotion");
