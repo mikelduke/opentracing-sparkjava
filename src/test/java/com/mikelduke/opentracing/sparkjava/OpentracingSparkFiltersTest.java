a80812ecce450628737e1639cf29beee7556e907
mikel duke <mikelduke@gmail.com>
2018-06-09 12:03:53 -0500

    unit test for some tags

--- src/test/java/com/mikelduke/opentracing/sparkjava/OpentracingSparkFiltersTest.java
+++ src/test/java/com/mikelduke/opentracing/sparkjava/OpentracingSparkFiltersTest.java
@@ -3,60 +3,69 @@ package com.mikelduke.opentracing.sparkjava;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import io.jaegertracing.Tracer;
 import io.jaegertracing.samplers.ConstSampler;
 import io.jaegertracing.samplers.Sampler;
+import io.opentracing.tag.Tags;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import spark.Spark;
 
 public class OpentracingSparkFiltersTest {
 
-    private DummyJaegerReporter reporter = new DummyJaegerReporter();
+    private static DummyJaegerReporter reporter = new DummyJaegerReporter();
 
-    private Sampler sampler = new ConstSampler(true);
+    private static Sampler sampler = new ConstSampler(true);
 
-    private Tracer tracer = new Tracer.Builder("sparkjava-test")
+    private static Tracer tracer = new Tracer.Builder("sparkjava-test")
             .withReporter(reporter)
             .withSampler(sampler)
             .build();
 
-    OkHttpClient client = new OkHttpClient.Builder()
+    private static OkHttpClient client = new OkHttpClient.Builder()
             .connectTimeout(15, TimeUnit.SECONDS)
             .readTimeout(15, TimeUnit.SECONDS)
             .build();
 
-    @Before
-    public void before() {
-        reporter.setSpan(null);
-        Spark.init();
-        Spark.awaitInitialization();
-    }
+    private static String host = "";
 
-    @After
-    public void after() {
-        Spark.stop();
-    }
-
-    @Test
-    public void spanIsNotNull() throws IOException {
+    @BeforeClass
+    public static void beforeClass() {
         OpenTracingSparkFilters sparkTracingFilters = new OpenTracingSparkFilters(tracer);
         Spark.before(sparkTracingFilters.before());
 		Spark.afterAfter(sparkTracingFilters.afterAfter());
         Spark.exception(Exception.class, sparkTracingFilters.exception());
+
         Spark.get("/", (req, res) -> "hello");
-        
+        Spark.get("/error", (req, res) -> {
+			throw new RuntimeException("test exceotion");
+		});
+
         Spark.awaitInitialization();
 
-        int port = Spark.port();
-        
+        host = "http://localhost:" + Spark.port() + "/";
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        Spark.stop();
+    }
+
+    @Before
+    public void beforeEach() {
+        reporter.setSpan(null);
+    }
+    
+    @Test
+    public void spanIsNotNull() throws IOException {
         Request request = new Request.Builder()
-                .url("http://localhost:" + port + "/hello")
+                .url(host + "/hello")
                 .build();
 
         client.newCall(request).execute();
@@ -64,19 +73,38 @@ public class OpentracingSparkFiltersTest {
         Assert.assertNotNull(reporter.getSpan());
     }
 
-    // @Test
-    // public void spanIsNullWithoutFilters() throws IOException {
-    //     Spark.get("/", (req, res) -> "hello");
-    //     Spark.awaitInitialization();
+    @Test
+    public void spanHasHttpMethod() throws IOException {
+        Request request = new Request.Builder()
+                .url(host + "/hello")
+                .build();
 
-    //     int port = Spark.port();
-        
-    //     Request request = new Request.Builder()
-    //             .url("http://localhost:" + port + "/hello")
-    //             .build();
+        client.newCall(request).execute();
 
-    //     client.newCall(request).execute();
+        Assert.assertTrue("GET".equalsIgnoreCase((String) reporter.getSpan().getTags().get(Tags.HTTP_METHOD.getKey())));
+    }
 
-    //     Assert.assertNull(reporter.getSpan());
-    // }
+    @Test
+    public void spanShowsError() throws IOException {
+        Request request = new Request.Builder()
+                .url(host + "/error")
+                .build();
+
+        client.newCall(request).execute();
+
+        Assert.assertTrue(Boolean.TRUE.equals((Boolean) reporter.getSpan().getTags().get(Tags.ERROR.getKey())));
+    }
+
+    @Test
+    public void spanHasUrl() throws IOException {
+        Request request = new Request.Builder()
+                .url(host + "/hello")
+                .build();
+
+        client.newCall(request).execute();
+
+        String url = (String) reporter.getSpan().getTags().get(Tags.HTTP_URL.getKey());
+
+        Assert.assertTrue(url.contains("hello"));
+    }
 }
